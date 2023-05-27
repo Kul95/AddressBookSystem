86c02717253066b727d7ce28061ee2daa37a8728
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-27 12:54:03 +0530

    Added : Contact-class-variables

---
+++ src/com/bridglabz/AddressBookTask/Contact.java
@@ -0,0 +1,12 @@
+package com.bridglabz.AddressBookTask;
+
+public class Contact {
+    public String firstName;
+    public String lastName;
+    public String address;
+    public String city;
+    public String state;
+    public int zip;
+    public int phoneNumber;
+    public int email;
+}
