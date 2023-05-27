9eac1e862560c5cf70cd08a5b61255098187738b
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-27 12:43:25 +0530

    Added :Start displaying AddressBook

---
+++ src/com/bridglabz/AddressBookTask/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridglabz.AddressBookTask;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook program. ");
+    }
+}
